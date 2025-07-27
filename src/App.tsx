50a7b2fd561bc1cd850fea0160d815911609382e
bernard-rodrigues <bernard.rodrigues@outlook.com>
2025-07-27 00:47:00 -0300

    add: crabs are detailed and scenario has a new animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Board } from "./components/Board"
 import { TurnContext } from "./contexts/TurnContext";
+import { TurnTextbox } from "./components/TurnTextbox";
 
 export const App = () => {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
   const [currentPlayer, setCurrentPlayer] = useState(() => Math.random() < 0.5 ? 1 : 2);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+    
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   const togglePlayer = () => {
     setCurrentPlayer(currentPlayer === 1 ? 2 : 1); 
@@ -15,22 +29,15 @@ export const App = () => {
       togglePlayer
     }}>
       <main 
-        className="relative h-screen w-screen font-ribeye"
+        className="relative h-screen w-screen font-ribeye transition-all duration-500"
         style={{
-          backgroundImage: 
-            currentPlayer === 1 ? 
-            "linear-gradient(to bottom, hsl(211, 100%, 80%), hsl(211, 100%, 65%))" :
-            "linear-gradient(to bottom, hsl(354, 70%, 83%), hsl(354, 70%, 68%))",
+          backgroundImage: "linear-gradient(to right, hsl(211, 100%, 80%) 0%, hsl(211, 100%, 65%) 50%, hsl(354, 70%, 68%) 50%, hsl(354, 70%, 83%) 100%)",
+          backgroundSize: "200% 100%",
+          backgroundPosition: `${currentPlayer === 1 ? "0% 0%" : "100% 0%"}`
         }}
       >
-        <h1 className="
-          text-main-text text-4xl md:text-6xl text-center 
-          w-full absolute top-1/8 left-1/2 -translate-x-1/2
-          md:top-16
-          lg:text-left lg:translate-x-0 lg:w-auto lg:left-8 lg:text-3xl
-        ">
-          Player {currentPlayer} turn
-        </h1>
+        <TurnTextbox currentPlayer={currentPlayer} screenWidth={screenWidth} textBoxOwner={1}/>
+        <TurnTextbox currentPlayer={currentPlayer} screenWidth={screenWidth} textBoxOwner={2}/>
         <Board />
       </main>
     </TurnContext.Provider>
--- src/components/Crab.tsx
+++ src/components/Crab.tsx
@@ -1,4 +1,6 @@
+import { useContext, useState } from "react";
 import type { CrabObject } from "../types/types"
+import { TurnContext } from "../contexts/TurnContext";
 
 interface CrabProps{
     crab: CrabObject
@@ -6,25 +8,37 @@ interface CrabProps{
 }
 
 export const Crab = (props: CrabProps) => {
+    const {currentPlayer} = useContext(TurnContext);
+    
+    const [isHovered, setIsHovered] = useState(false);
+    
+    const crabStyle = {
+        left: `${(props.crab.x/6)*100}%`,
+        top: `${(props.crab.y/6)*100}%`,
+
+        transform: `scale(${props.crab.active ? 1 : props.crab.player === currentPlayer ? 0.9 : 0.8})`,
+        outline: props.crab.active ? `2px solid ${
+            props.crab.player === 1 ? 
+            "hsla(211, 100%, 50%, 50%)" : 
+            "hsla(354, 70%, 53%, 50%)"
+        }` : `0px solid transparent`,
+        outlineOffset: props.crab.active ? "8px" : "0px",
+    }
+
+    const hoverStyle = {
+        filter: "brightness(125%)"
+    }
+    
     return(
         <div 
             className={`
                 absolute w-1/6 h-1/6 rounded-full shadow-md shadow-zinc-600 transition-all duration-150
                 bg-linear-to-r ${props.crab.player === 1 ? "from-crab-blue-left to-crab-blue-right" : "from-crab-red-left to-crab-red-right"}
-                hover:brightness-125
             `}
-            style={{
-                left: `${(props.crab.x/6)*100}%`,
-                top: `${(props.crab.y/6)*100}%`,
-                transform: props.crab.active ? 'scale(1)' : 'scale(0.9)',
-                outline: props.crab.active ? `2px solid ${
-                    props.crab.player === 1 ? 
-                    "hsla(211, 100%, 50%, 50%)" : 
-                    "hsla(354, 70%, 53%, 50%)"
-                }` : `0px solid transparent`,
-                outlineOffset: props.crab.active ? "8px" : "0px",
-            }}
+            style={isHovered && currentPlayer === props.crab.player ? { ...crabStyle, ...hoverStyle } : crabStyle}
             onClick={() => props.handleCrabActiveFunction(props.crab)}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
         />
     );
 }
