09db1a272663e0dee513ccbc745040f0d5cc5581
Bernard da Silva Rodrigues <bernard.rodrigues@caed.local>
2025-07-30 15:18:09 -0300

    fix: Repeat button not working on state 4

--- src/components/Repeat.tsx
+++ src/components/Repeat.tsx
@@ -3,14 +3,14 @@ import repeat from "../assets/images/repeat.svg";
 import { TurnContext } from "../contexts/TurnContext";
 
 export const Repeat = () => {
-    const { handleReset } = useContext(TurnContext);
+    const { handleReset, gameState, handleGameStateChange, isAIMode } = useContext(TurnContext);
     
     return (
         <img 
             src={repeat} 
             alt="Repeat sign" 
             className="fixed left-3 top-3 w-10 md:w-14 lg:w-12 hover:cursor-pointer" 
-            onClick={() => handleReset(true)} 
+            onClick={gameState === 4 ? () => (handleGameStateChange(isAIMode ? 2 : 1)) : () => handleReset(true)}
         />
     );
 }
